868cd8d1e074eac03b33c4d787a9c722f92c5d0e
Rajiv Senthilnathan <rajivsen@ca.ibm.com>
2019-09-13 16:09:38 -0400

    Create idp scripts

--- kubejob/job.go
+++ kubejob/job.go
@@ -11,14 +11,20 @@ import (
 // CreateBuildTaskKubeJob creates a Kubernetes Job
 func CreateBuildTaskKubeJob(buildTaskJob string, taskName string, namespace string, idpClaimName string, projectSubPath string, projectName string) (*batchv1.Job, error) {
 	fmt.Printf("Creating job %s\n", buildTaskJob)
-	// Create a Kube job to run mvn compile for a Liberty project
-	mvnCommand := "echo listing /data/idp/src && ls -la /data/idp/src && echo copying /data/idp/src to /tmp/app && cp -rf /data/idp/src /tmp/app && echo chown, listing and running mvn in /tmp/app: && chown -fR 1001 /tmp/app && cd /tmp/app && ls -la && mvn -B clean package -Dmaven.repo.local=/data/idp/cache/.m2/repository -DskipTests=true && echo copying target to output dir && rm -rf /data/idp/output && mkdir -p /data/idp/output && cp -rf /tmp/app/target /data/idp/output && chown -fR 1001 /data/idp/output && echo listing /data/idp/output after mvn and chown 1001 buildoutput && ls -la /data/idp/output/target && echo rm -rf /data/idp/buildartifacts and copying artifacts && rm -rf /data/idp/buildartifacts && mkdir -p /data/idp/buildartifacts/ && cp -r /data/idp/output/target/liberty/wlp/usr/servers/defaultServer/* /data/idp/buildartifacts/ && cp -r /data/idp/output/target/liberty/wlp/usr/shared/resources/ /data/idp/buildartifacts/ && cp /data/idp/src/src/main/liberty/config/jvmbx.options /data/idp/buildartifacts/jvm.options && echo chown the buildartifacts dir && chown -fR 1001 /data/idp/buildartifacts"
+	// Create a Kube job to run maven builds
 
-	if taskName == "inc" {
-		mvnCommand = "echo listing /data/idp/src && ls -la /data/idp/src && echo copying /data/idp/src to /tmp/app && cp -rf /data/idp/src /tmp/app && echo chown, listing and running mvn in /tmp/app: && chown -fR 1001 /tmp/app && cd /tmp/app && ls -la && mvn -B clean package -Dmaven.repo.local=/data/idp/cache/.m2/repository -DskipTests=true && echo copying target to output dir && rm -rf /data/idp/output && mkdir -p /data/idp/output && cp -rf /tmp/app/target /data/idp/output && chown -fR 1001 /data/idp/output && echo listing /data/idp/output after mvn and chown 1001 buildoutput && ls -la /data/idp/output/target && echo copying artifacts && cp -rf /data/idp/output/target/liberty/wlp/usr/servers/defaultServer/apps/* /data/idp/buildartifacts/apps/ && echo chown the buildartifacts apps dir && chown -fR 1001 /data/idp/buildartifacts/apps"
+	/*
+	 * NOTE: The scripts must be manually copied to the volume for the POC, in the actual implementation the scripts would be either in the volume
+	 * or in the container as part of the sync step
+	 */
+	command := "/data/idp/scripts/build-container-full.sh"
+
+	if taskName == "update" {
+		command = "/data/idp/scripts/build-container-update.sh"
 	}
 
-	fmt.Printf("Mvn Command: %s\n", mvnCommand)
+	fmt.Printf("Task name: %s\n", taskName)
+	fmt.Printf("Command: %s\n", command)
 	backoffLimit := int32(1)
 	parallelism := int32(1)
 	job := &batchv1.Job{
@@ -45,7 +51,7 @@ func CreateBuildTaskKubeJob(buildTaskJob string, taskName string, namespace stri
 							Image:           "docker.io/maven:3.6",
 							ImagePullPolicy: corev1.PullAlways,
 							Command:         []string{"/bin/sh", "-c"},
-							Args:            []string{mvnCommand},
+							Args:            []string{command},
 							VolumeMounts: []corev1.VolumeMount{
 								{
 									Name:      "idp-volume",
---
+++ tmp/copy-scripts-to-copypod.sh
@@ -0,0 +1,4 @@
+kubectl cp build-volume-full.sh copy-pod-mock:/data/idp/bin
+kubectl cp build-volume-update.sh copy-pod-mock:/data/idp/bin
+kubectl cp build-container-full.sh copy-pod-mock:/data/idp/bin
+kubectl cp build-container-update.sh copy-pod-mock:/data/idp/bin
